208bc47afdc402dd53e4c21f10edf2c72aeed235
Chris von Krogh <cvonkrogh@paypal.com>
2015-03-12 15:23:53 -0700

    added mad libs

--- Play.py
+++ Play.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python
 # simple choose-your-own-adventure game
 import sys, time, random
+N = ['poop', 'toilet', 'dinosaur']
+V = ['run', 'poop', 'pee']
+A = ['smelly', 'dark', 'slimy', 'cold', 'disgusting']
 LEVELS = [
-    { 'intro' : 'You enter a room. There are many ways forward.\n', 'choices' : ['left', 'right', 'straight'] },
-    { 'intro' : 'You are now on the roof. It is dark outside. How do you want to proceed?\n', 'choices' : ['look up', 'look at the next building over'] },
-    { 'intro' : 'It is too dark to see clearly.\n', 'choices' : ['go back inside through the door', 'climb back in through the window'] },
-    { 'intro' : 'It was difficult, but you are back inside. You hear a noise from downstairs.\n', 'choices' : ['put your ear to the floor', 'go to the top of the stairs to listen'] },
-    { 'intro' : 'It\'s music that you hear!\n', 'choices' : ['Start singing', 'Try to whistle'] },
+    { 'intro' : 'You enter a room. There are many ways forward.\n', 'subs' : [], 'choices' : ['left', 'right', 'straight'] },
+    { 'intro' : 'You are now on the roof. It is %s outside. You kind of want to %s but that is not an option. How do you want to proceed?\n', 'subs' : [A, V], 'choices' : ['look up', 'look at the next building over'] },
+    { 'intro' : 'It is too %s to see clearly.\n', 'subs' : [A], 'choices' : ['go back inside through the door', 'climb back in through the window'] },
+    { 'intro' : 'It was difficult, but you are back inside. You hear a noise from downstairs.\n', 'subs' : [], 'choices' : ['put your ear to the floor', 'go to the top of the stairs to listen'] },
+    { 'intro' : 'It\'s music that you hear!\n', 'subs' : [], 'choices' : ['Start singing', 'Try to whistle'] },
 ]
 FINAL_OUTCOMES = [lambda x : lambda : slow_print("you win\n"), lambda x : lambda : slow_print("you lose\n")]
 OUTCOMES = FINAL_OUTCOMES + [lambda x : generate_dictionary(x)]
 OUTCOME_ODDS = [3, 2, 8]
 def slow_print(s):
     for letter in s: sys.stdout.write(letter) ; sys.stdout.flush() ; time.sleep(.04)
+def mad_libs(s, sublists):
+    return s % tuple([random.choice(sublist) for sublist in sublists])
 def generate_dictionary(level):
     d = {}
-    d['sentence']=LEVELS[level]['intro']
+    raw_sentence = LEVELS[level]['intro']
+    subs = LEVELS[level]['subs']
+    if len(subs) > 0:
+        sentence = mad_libs(raw_sentence, subs)
+    else:
+        sentence = raw_sentence
+    d['sentence']=sentence
     d['options']={}
     outcomes_raw = FINAL_OUTCOMES if level == len(LEVELS)-1 else OUTCOMES
     outcome_functions = []
@@ -28,7 +39,7 @@ def generate_dictionary(level):
     return d
 def play_dict(d):
     slow_print(d['sentence'])
-    slow_print("here are your choices\n")
+    slow_print("Here are your choices:\n")
     for k in d['options'].keys(): print k
     slow_print("choose carefully ==> ")
     choice = raw_input()
@@ -41,6 +52,8 @@ def play_dict(d):
     else:
         subtree()
 def main():
+    #slow_print("")
     d = generate_dictionary(0)
     play_dict(d)
+    #slow_print(mad_libs("You see a %s %s.\n", [A, N]))
 main()
